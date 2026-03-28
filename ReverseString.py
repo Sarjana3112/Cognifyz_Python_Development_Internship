8b1f5731178f245c6410bb8d8e4dfa260de8acf8
V S SARJANA <sarjanavs47@gmail.com>
2026-03-28 08:22:07 +0530

    Create ReverseString.py

---
+++ ReverseString.py
@@ -0,0 +1,8 @@
+def reverse_string(text):
+    return text[::-1]
+
+# User Input
+user_input = input("Enter a string: ")
+reversed_text = reverse_string(user_input)
+
+print("Reversed String:", reversed_text)
